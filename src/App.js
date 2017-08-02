a976173bbfcbe3bec4cab383724ab8714d203b03
Jesus Amaya <jesus.amaya@rulesware.com>
2017-08-02 17:58:31 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    campo1 : "dos"
+  }
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
